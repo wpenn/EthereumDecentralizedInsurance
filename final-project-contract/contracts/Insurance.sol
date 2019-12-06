638104234d6f815daaf14fec67c350776415f972
wesleypenn <wesley.penn@extcare.com>
2019-12-06 00:16:45 -0500

    removed front-end skeleton, removed api keys from current version

--- final-project-contract/contracts/Insurance.sol
+++ final-project-contract/contracts/Insurance.sol
@@ -4,11 +4,12 @@ import "./provableAPI.sol";
 
 contract Insurance is usingProvable {
     //api
-    uint public gasPriceUSD; //TODO: Remove when replacing API
-    bytes32 public provableQueryId;
+    string APIKEY = "{YOUR-API-KEY}"; // https://openweathermap.org/
+    uint public tempReturned;
+    bytes32 provableQueryId;
     
     //contract terms
-    uint256 predictedPrice;
+    uint256 predictedTemp;
     uint256 toPayout; //Total winnings
     uint256 proposedDelay; // time the contract will end
     uint256 public payoutAmount; //Insurer Contribution
@@ -16,22 +17,22 @@ contract Insurance is usingProvable {
     string public zipcode; //Zipcode location
     
     //addresses
-    address payable public policyHolder; // address of the person b eing insured
-    address payable public insurer; //address of the insurer
+    address payable policyHolder; // address of the person b eing insured
+    address payable insurer; //address of the insurer
     
     //state of contract
     bool public proposed;
     bool public boughtIn;
-    uint256 public timeOfProposal; //TODO: remove public after testing
+    uint256 timeOfProposal;
     
     bool public policyHolderProposedKill; //Mechanism to kill contract
     bool public insurerProposedKill;
     
     constructor() public{
-        gasPriceUSD = 0; //TODO: remove when replacing API
+        tempReturned = 0;
         provableQueryId = bytes32(0);
         
-        predictedPrice = 0;
+        predictedTemp = 0;
         toPayout = 0;
         proposedDelay = 0;
         payoutAmount = 0;
@@ -49,14 +50,14 @@ contract Insurance is usingProvable {
         insurerProposedKill = false;
     }
     
-    function proposeContract(string memory _zipcode, uint256 _predictedPrice, uint256 _proposedDelay, uint256 _payoutAmount) public payable {
+    function proposeContract(string memory _zipcode, uint256 _predictedTemp, uint256 _proposedDelay, uint256 _payoutAmount) public payable {
         require(!proposed, "contract already proposed");
         require(msg.value > 0, "You need to put in some ether");
         policyHolder = msg.sender;
         premiumAmount = msg.value;
         payoutAmount = _payoutAmount;
         toPayout = premiumAmount;
-        predictedPrice = _predictedPrice;
+        predictedTemp = _predictedTemp;
         zipcode = _zipcode;
         
         proposedDelay = _proposedDelay;
@@ -69,7 +70,7 @@ contract Insurance is usingProvable {
         require(proposed, "You need someone to propose a contract");
         require(!isContractExpired(), "Contract has expired, to return value from escrow please call proposeContractKill.");
         require(msg.value == payoutAmount, "You need to put in the same amount of ether as the contract requireds");
-        require(msg.sender != policyHolder, "You can't buy into the contract if you proposed it."); //Remove for testing purposes
+        require(msg.sender != policyHolder, "You can't buy into the contract if you proposed it."); //TODO: Remove for testing purposes
         insurer = msg.sender;
         toPayout = toPayout + payoutAmount;
             
@@ -77,18 +78,18 @@ contract Insurance is usingProvable {
             
         //Oracle API call
         uint256 secondsToExpiration = (timeOfProposal + proposedDelay) - now; //time until contract expires in seconds
-        string memory restCall = string(abi.encodePacked("json(http://api.openweathermap.org/data/2.5/weather?zip=", string(zipcode), ",us&APPID=201d8f5eb38a067fce3ba183d9826aed).main.temp"));
+        string memory restCall = string(abi.encodePacked("json(http://api.openweathermap.org/data/2.5/weather?zip=", string(zipcode), ",us&APPID=", APIKEY, ").main.temp"));
         provableQueryId = provable_query(secondsToExpiration, "URL", restCall); //TODO: change url when replacing API
     }
     
     function __callback(bytes32 _myid, string memory _result) public { //Oracle native function (Oracle rest call returning information to contract)
         require(msg.sender == provable_cbAddress());
-        gasPriceUSD = parseInt(_result, 2);
+        tempReturned = parseInt(_result, 2);
         
         if (toPayout > address(this).balance) { //REVIEW
             toPayout = address(this).balance;
         }
-        if (gasPriceUSD > predictedPrice) { //TODO: fix statements with new api
+        if (tempReturned > predictedTemp) { //TODO: fix statements with new api
             policyHolder.transfer(toPayout);
         } else {
             insurer.transfer(toPayout);
@@ -97,7 +98,7 @@ contract Insurance is usingProvable {
     }
     
     function clearContract() private{
-        predictedPrice = 0;
+        predictedTemp = 0;
         toPayout = 0;
         proposedDelay = 0;
         payoutAmount = 0;
